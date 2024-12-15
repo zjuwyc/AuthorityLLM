e79cd4c30de9750b1d1076511f50e71bafe7a1a2
zjuwyc <12421031@zju.edu.cn>
2024-12-15 08:05:13 +0800

    Update make_all_feature_datasets.sh

--- make_all_feature_datasets.sh
+++ make_all_feature_datasets.sh
@@ -1,33 +1,27 @@
 #!/bin/bash
 
 python make_feature_datasets.py \
-    --feature_collection programming_lang_id
+    --feature_collection cb
 
 python make_feature_datasets.py \
-    --feature_collection natural_lang_id \
+    --feature_collection copa \
     --ignore_first_k 1 \
     --lang_id_n_tokens 2
 
 python make_feature_datasets.py \
-    --feature_collection distribution_id
+    --feature_collection wic
 
 python make_feature_datasets.py \
-    --feature_collection position \
+    --feature_collection wng \
     --n_seqs 10000 \
     --seq_len 1024
 
 python make_feature_datasets.py \
-    --feature_collection text_features \
+    --feature_collection wsc \
     --seq_len 256 \
     --n_seqs 10000
 
 python make_feature_datasets.py \
     --feature_collection ewt
 
-python make_feature_datasets.py \
-    --feature_collection compound_words \
-    --seq_len 24
 
-python make_feature_datasets.py \
-    --feature_collection latex \
-    --seq_len 1024
